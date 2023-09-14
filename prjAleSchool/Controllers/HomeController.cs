28c3bcd4aea56ba8b54181043b66255136328917
jeff <jeffercs96@gmail.com>
2023-09-13 23:19:46 -0500

    proyecto incial

---
+++ prjAleSchool/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace prjAleSchool.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet(Name = "GetWeatherForecastss")]
+        public IActionResult Get()
+        {
+            return Ok(new { message = " hola" });
+            }
+    }
+}
+
